d98a9c58a5b07b552372f11793d9a415367521d5
Zahid-isu <87206801+Zahid-isu@users.noreply.github.com>
2022-09-18 15:19:56 -0500

    edited q_5

--- zahid/assignment_1/q5.py
+++ zahid/assignment_1/q5.py
@@ -2,14 +2,36 @@
 
 #     def extime(self, method):
 #         self.method= method
+#get time to run q4 
 
-
+from functools import wraps
+import time
 from q4 import chapter_num 
 obj =chapter_num()
 method= obj.chp_num
-n=4
 
-print(method(n))
+#timeit_wrapper function
+def timeit(meth):
+    @wraps(meth)
+    def timeit_wrapper(*args, **kwargs):
+        start = time.time()
+        result = meth(*args, **kwargs)
+        print(result)  #display results 
+        end = time.time()
+        total_time = end - start
+        print(f'\n method executes in {total_time:.8f} seconds')
+    return timeit_wrapper
 
+class Calculator:
+    @timeit
+    def get_time(self, num):
+
+        result= method(num)
+        return result
+
+
+if __name__ == '__main__':
+    calc = Calculator()
+    calc.get_time(10)
 
 
