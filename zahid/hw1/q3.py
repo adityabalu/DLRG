cf6f3c0f4993e7e944c2b113f56acd8894ff6f25
zahid <87206801+zahid-isu@users.noreply.github.com>
2023-03-23 01:08:49 -0500

    hw1 files

---
+++ zahid/hw1/q3.py
@@ -0,0 +1,34 @@
+class match_product():
+
+    def mat_pro(self, a,b):
+        self.a =a
+        self.b =b
+        out =[]
+
+        for x,y in zip(a,b):
+
+            if isinstance(x, str)  and isinstance(y, str): # both str
+                if x.isdigit() and y.isdigit(): #both number
+                    out.append(int(x)*int(y))
+            elif (isinstance(x, int) and isinstance(y, str)):
+                if y.isdigit():
+                    out.append(int(x)*int(y))
+            elif (isinstance(x, str) and isinstance(y, int)):
+                if x.isdigit():
+                    out.append(int(x)*int(y))
+            else:
+                continue
+        return out
+
+
+method= match_product()
+#inputs
+a = ['12', '13', 'Hello', '4', '6', '10]']
+b = ['2', '3', '4', 5, '[6', 12, 14]
+
+
+if __name__== "__main__":
+    print(method.mat_pro(a,b))
+
+# sample output:
+# [24, 39, 20]
